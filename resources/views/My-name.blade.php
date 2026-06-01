ea425221eda5a346a9d82c7c7477fe123984b6e3
charles134031 <prettyshayradalida@gmail.com>
2026-06-01 10:04:08 +0800

    uploaded whole laravel project

---
+++ resources/views/My-name.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>My First Laravel Page</title>
+</head>
+<body>
+    <h1>Hello, DICT Training Day 1!</h1>
+    <p>My name is <strong>[Your Name Here]</strong>, and this is my very first custom Laravel page.</p>
+</body>
+</html>
